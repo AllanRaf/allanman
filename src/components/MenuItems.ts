d4bcc514522b91fae2aeb424c37c0a443ff69017
AllanRaf <rafale.uk@googlemail.com>
2020-12-21 23:20:07 +0100

    Updates

--- src/components/MenuItems.ts
+++
@@ -1,6 +0,0 @@
-export const MenuItems = [
-  { title: "Home", path: "/marketing", cName: "dropdown-link" },
-  { title: "Stack", path: "/consulting", cName: "dropdown-link" },
-  { title: "About", path: "/design", cName: "dropdown-link" },
-  { title: "Contact Me", path: "/development", cName: "dropdown-link" },
-];
